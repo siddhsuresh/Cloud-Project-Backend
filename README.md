9249b197c960c80211601d3536fb56a4afa2ddf8
Siddharth Suresh <siddharth.suresh2020@vitstudent.ac.in>
2022-04-05 10:54:10 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# DRTS-Project-Backend
+# Real Time Distributed Irrigation Project
+## Socket.io Fastify Web Server
+## CSE2021 DRTS J Component 
+## 20BPS1042 Siddharth Suresh 
+
+Creating the Websocket and REST API to be consumed by the Next.js website for the project
---
+++ src/build.js
@@ -0,0 +1,55 @@
+/* eslint strict:"off" */
+"use strict";
+const fastify = require("fastify");
+const {
+  socketRoutes,
+  allHeatReadings,
+  allSoilReadins,
+  latestReadings
+} = require("./socket.js");
+
+function build(opts) {
+  const app = fastify(opts);
+
+  socketRoutes(app, {
+    cors: {
+      origin: "*"
+    }
+  });
+
+  app.get("/", async (request, reply) => {
+    return { "CSE2021 DRTS Project API": "20BPS1042 Siddharth Suresh" };
+  });
+
+  app.get(
+    "/API",
+    {
+      query: {
+        q: {
+          type: "string"
+        }
+      }
+    },
+    async (request, reply) => {
+      const { q } = request.query;
+      if (q && q === "latest") {
+        console.log(latestReadings);
+        return latestReadings;
+      }
+      if (q && q === "soil") {
+        return allSoilReadins;
+      }
+      if (q && q === "dht") {
+        return allHeatReadings;
+      }
+      return {
+        error: "404 - No Such Page In The Api"
+      };
+    }
+  );
+  return app;
+}
+
+module.exports = {
+  build
+};
