709111f394dfe7fe3c81d8464d0d54fde543e71b
Siddharth Suresh <siddharth.suresh2020@vitstudent.ac.in>
2022-04-18 22:39:01 +0530

    added a time endpoint

--- src/build.js
+++ src/build.js
@@ -81,7 +81,7 @@ function build(opts) {
   );
   app.get("/currentTime", async (request, reply) => {
     return {
-      currentTime: new Date().toLocaleTimeString("en-IN")
+      currentTime: new Date()
       };
   });
   return app;
